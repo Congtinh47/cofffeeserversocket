15a7d08548ae864708aa9b3609c63b9478c95e4f
Congtinh47 <71366924+Congtinh47@users.noreply.github.com>
2022-01-03 16:57:37 +0700

    dfsafadf

--- src/socket.ts
+++ src/socket.ts
@@ -1,13 +1,8 @@
 import { nanoid } from "nanoid";
 import { Server, Socket } from "socket.io";
-import logger from "./utils/logger";
 
 function socket({ io }: { io: Server }) {
-	logger.info(`Sockets enabled`);
-
 	io.on("connection", (socket: Socket) => {
-		logger.info(`User connected ${socket.id}`);
-
 		socket.on("datatest", ({ test, orderId }) => {
 			socket.emit("afterCheck", {
 				message: "thanh toan thanh cong",
